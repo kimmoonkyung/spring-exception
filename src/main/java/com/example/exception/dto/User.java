fdb056adf0389976a4e594d931add7a82a6226c1
mxxnkyung@naver.com <mxxnkyung@naver.com>
2021-06-06 16:27:07 +0900

    21.06.06 Exception 처리

--- src/main/java/com/example/exception/dto/User.java
+++ src/main/java/com/example/exception/dto/User.java
@@ -1,12 +1,23 @@
 package com.example.exception.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
+@Builder
 public class User {
 
+    @NotEmpty
+    @Size(min = 2, max = 10)
     private String name;
-    private int age;
 
+    @Min(1)
+    @NotNull
+    private Integer age;
 
 }
