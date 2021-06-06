dd5204c623b5e1fe61aae81c64b5798849d9e251
mxxnkyung@naver.com <mxxnkyung@naver.com>
2021-06-06 17:01:11 +0900

    21.06.06 Exception 처리

--- src/main/java/com/example/exception/advice/ApiControllerAdvice.java
+++ src/main/java/com/example/exception/advice/ApiControllerAdvice.java
@@ -1,19 +1,18 @@
 package com.example.exception.advice;
 
+import com.example.exception.controller.ApiController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice()
-public class GlobalControllerAdvice {
+@RestControllerAdvice(basePackageClasses = ApiController.class)
+public class ApiControllerAdvice {
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity exception(Exception e){
         System.out.println(e.getClass().getName());
-        System.out.println(e.getLocalizedMessage());
-
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("");
     }
 
--- src/main/java/com/example/exception/controller/ApiController.java
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -3,17 +3,38 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated
 public class ApiController {
 
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age) {
+    public User get(
+            @Size(min = 2, max = 10)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age) {
         User user = User.builder()
                 .name(name)
                 .age(age)
@@ -32,8 +53,60 @@ public class ApiController {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        System.out.println("api controller");
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        bindingResult.getAllErrors().forEach(error -> {
+            FieldError field = (FieldError) error;
+
+            String fieldName = field.getField();
+            String message = field.getDefaultMessage();
+            String value = field.getRejectedValue().toString();
+
+            System.out.println("-------------------");
+            System.out.println(fieldName);
+            System.out.println(message);
+            System.out.println(value);
+            System.out.println("-------------------");
+        });
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+
+    @ExceptionHandler(value = ConstraintViolationException.class)
+    public ResponseEntity constraintViolationException(ConstraintViolationException e) {
+        e.getConstraintViolations().forEach(error -> {
+
+            Stream<Path.Node> stream = StreamSupport.stream(error.getPropertyPath().spliterator(), false);
+            List<Path.Node> list = stream.collect(Collectors.toList());
+
+            String field = list.get(list.size() - 1).getName();
+            String message = error.getMessage();
+            String invalidValue = error.getInvalidValue().toString();
+
+            System.out.println("-------------------");
+            System.out.println(field);
+            System.out.println(message);
+            System.out.println(invalidValue);
+            System.out.println("-------------------");
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = MissingServletRequestParameterException.class)
+    public ResponseEntity missingServletRequestParameterException(MissingServletRequestParameterException e) {
+
+        String fieldName = e.getParameterName();
+        String fieldType = e.getParameterType();
+        String invalidValue = e.getMessage();
+
+        System.out.println(fieldName);
+        System.out.println(fieldType);
+        System.out.println(invalidValue);
+
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
 
 }
